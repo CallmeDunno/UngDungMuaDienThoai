bea1f0f8a4f93a2c14a03507016c717d64da3dac
Dunno <dungworkit@gmail.com>
2023-03-08 21:14:29 +0700

    Dungx

--- app/src/main/java/com/example/qlbdt/fActivity/RegisterAccountActivity.java
+++ app/src/main/java/com/example/qlbdt/fActivity/RegisterAccountActivity.java
@@ -1,27 +1,63 @@
 package com.example.qlbdt.fActivity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.qlbdt.R;
+import com.example.qlbdt.fDatabase.MyDatabase;
+import com.example.qlbdt.fEnum.EDesSmartphoneDetail;
+import com.example.qlbdt.fObject.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.regex.Pattern;
 
 public class RegisterAccountActivity extends AppCompatActivity {
 
-    ImageView img_arrow_left;
+    EditText edt_name, edt_phone, edt_email, edt_addr;
+    TextView tv_w_name, tv_w_phone, tv_w_email, tv_w_addr, tv_w_r_phone, tv_w_r_email;
+    ImageView img_avt;
+    Button btn_confirm;
 
-    private void init(){
-        img_arrow_left = findViewById(R.id.img_arrow_left);
-
-        img_arrow_left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterAccountActivity.this, LoginActivity.class));
-            }
-        });
+    private final int MY_CODE_REQUEST_FOLDER = 100;
+    private MyDatabase database;
+    private void init() {
+        edt_name = findViewById(R.id.edt_username_ca);
+        edt_phone = findViewById(R.id.edt_phone_ca);
+        edt_email = findViewById(R.id.edt_email_ca);
+        edt_addr = findViewById(R.id.edt_address_ca);
+        img_avt = findViewById(R.id.img_avt_user_ca);
+        btn_confirm = findViewById(R.id.btn_confirm);
+        tv_w_name = findViewById(R.id.tv_label_warning_1_ca);
+        tv_w_phone = findViewById(R.id.tv_label_warning_2_ca);
+        tv_w_email = findViewById(R.id.tv_label_warning_3_ca);
+        tv_w_addr = findViewById(R.id.tv_label_warning_4_ca);
+        tv_w_r_phone = findViewById(R.id.tv_label_warning_regex_1_ca);
+        tv_w_r_email = findViewById(R.id.tv_label_warning_regex_2_ca);
     }
 
     @Override
@@ -30,5 +66,210 @@ public class RegisterAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_account);
 
         init();
+
+        database = new MyDatabase(RegisterAccountActivity.this, "MuaBanDienThoai.sqlite", null, 1);
+
+        //region Insert Data
+        database.QueryDatabase("INSERT INTO Brand VALUES(NULL, 'Apple')");
+        database.QueryDatabase("INSERT INTO Brand VALUES(NULL, 'Samsung')");
+        database.QueryDatabase("INSERT INTO Brand VALUES(NULL, 'Oppo')");
+        database.QueryDatabase("INSERT INTO Brand VALUES(NULL, 'Nokia')");
+        database.QueryDatabase("INSERT INTO Brand VALUES(NULL, 'Vivo')");
+        database.QueryDatabase("INSERT INTO Brand VALUES(NULL, 'Xiaomi')");
+
+        database.InsertSmartphone(RegisterAccountActivity.this, "iPhone 13 Pro Max", "31490000", 153,
+                R.drawable.apple_iphone_13_pro_max, 1);
+        database.InsertSmartphone(RegisterAccountActivity.this, "iPhone 14 Pro Max", "26490000", 98,
+                R.drawable.apple_iphone_14_promax, 1);
+        database.InsertSmartphone(RegisterAccountActivity.this, "iPhone 11", "11990000", 32,
+                R.drawable.iphone_11, 1);
+        database.InsertSmartphone(RegisterAccountActivity.this, "iPhone 12", "18490000", 54,
+                R.drawable.iphone12_xanh_duong, 1);
+        database.InsertSmartphone(RegisterAccountActivity.this, "iPhone 14 Plus", "23090000", 121,
+                R.drawable.iphone_14_plus, 1);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Nokia G11", "2690000", 31,
+                R.drawable.nokia_g11, 4);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Oppo Find X5 Pro", "24990000", 146,
+                R.drawable.oppo_find_x5_pro, 3);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Oppo Reno8 Pro 5G", "17590000", 253
+                , R.drawable.oppo_reno8_pro, 3);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Samsung Galaxy Z Fold3 5G",
+                "32990000", 195, R.drawable.samsung_galaxy_z_fold_3_5g, 2);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Samsung Galaxy S20 FE", "9490000",
+                31, R.drawable.samsung_galaxy_s20_fe_green, 2);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Samsung Galaxy S23 Ultra 5G",
+                "41990000", 257, R.drawable.samsung_galaxy_s23_sltra_plus, 2);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Samsung Galaxy Z Flip4", "19990000"
+                , 184, R.drawable.samsung_galaxy_zflip4_5g, 2);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Vivo V25 Pro 5G", "13290000", 43,
+                R.drawable.vivo_v25_pro_5g, 5);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Vivo Y21", "3290000", 52,
+                R.drawable.vivo_y21, 5);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Xiaomi 12T Pro 5G", "14090000", 57,
+                R.drawable.xiaomi_12t_pro, 6);
+        database.InsertSmartphone(RegisterAccountActivity.this, "Xiaomi Redmi Note 11", "4390000", 64,
+                R.drawable.xiaomi_redmi_note_11, 6);
+
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Apple A15 Bionic', '6GB', '1TB', 'Xanh nhạt', '4352mAh', '240g', '12 tháng', '1')",
+                EDesSmartphoneDetail.DES_i13prm.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Apple A16 Bionic', '6GB', '128GB', 'Tím', '3200mAh', '206g', '12 " +
+                        "tháng', '2')",
+                EDesSmartphoneDetail.DES_i14prm.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Apple A13 Bionic', '4GB', '64GB', 'Trắng', '3110mAh', '164g', '12 " +
+                        "tháng', '3')",
+                EDesSmartphoneDetail.DES_i11.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Apple A14 Bionic', '4GB', '256GB', 'Xanh dương', '2815mAh', '164g', '12 " +
+                        "tháng', '4')",
+                EDesSmartphoneDetail.DES_i12.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Apple A15 Bionic', '6GB', '128GB', 'Tím nhạt', '4325mAh', '203g', '12 " +
+                        "tháng', '5')",
+                EDesSmartphoneDetail.DES_i14p.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Unisoc T606', '4GB', '64GB', 'Xám', '5050mAh', '189g', '12 tháng'," +
+                        " '6')",
+                EDesSmartphoneDetail.DES_Nokia_G11.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 8 gen 1', '12GB', '256TB', 'Đen', '5000mAh', '218g'," +
+                        " '12 tháng', '7')",
+                EDesSmartphoneDetail.DES_Oppo_FindX5pro.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'MediaTek Dimensity 8100 Max 8 nhân', '12GB', '256GB', 'Xanh ngọc', '4500mAh', '183g', '12 tháng', '8')",
+                EDesSmartphoneDetail.DES_Oppo_Reno8pro.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 888', '12GB', '512GB', 'Xanh rêu', '4400mAh', '271g', '12 tháng', '9')",
+                EDesSmartphoneDetail.DES_SS_ZFOLD3.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 865', '8GB', '256GB', 'Xanh lá', '4500mAh', '190g', '12 tháng', '10')",
+                EDesSmartphoneDetail.DES_SS_S20FE.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 8 Gen 2 8 nhân', '12GB', '1TB', 'Tím  nhạt', '5000mAh', '223g', '12 tháng', '11')",
+                EDesSmartphoneDetail.DES_SS_S23U.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 8+ Gen 1', '8GB', '128GB', 'Tím', '3700mAh', '187g', '12 tháng', '12')",
+                EDesSmartphoneDetail.DES_SS_ZFLIP4.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'MediaTek Dimensity 1300', '8GB', '128GB', 'Xanh dương', '4830mAh', '190g', '12 tháng', '13')",
+                EDesSmartphoneDetail.DES_VV_V25pro.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'MediaTek Helio P35', '4GB', '64GB', 'Trắng', '5000mAh', '182g', '12 tháng', '14')",
+                EDesSmartphoneDetail.DES_VV_Y21.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 8+ Gen 1', '12GB', '256GB', 'Đen', '5000mAh', '205g', '12 tháng', '15')",
+                EDesSmartphoneDetail.DES_XM_12Tpro.getDes()));
+        database.QueryDatabase(String.format("INSERT INTO SmartphoneDetail VALUES(NULL, '%s', " +
+                        "'Snapdragon 680', '6GB', '128GB', 'Xanh dương đậm', '5000mAh', '179g', '12 tháng', '16')",
+                EDesSmartphoneDetail.DES_XM_N11.getDes()));
+        //endregion
+
+        ActivityResultLauncher<Intent> activityPickerResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK){
+                            Uri uri = result.getData().getData();
+                            try {
+                                InputStream inputStream = getContentResolver().openInputStream(uri);
+                                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                img_avt.setImageBitmap(bitmap);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+        img_avt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(RegisterAccountActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    activityPickerResultLauncher.launch(intent);
+                } else {
+                    ActivityCompat.requestPermissions(RegisterAccountActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_CODE_REQUEST_FOLDER);
+                }
+
+            }
+        });
+
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisableWarning();
+                if (CheckData()){
+                    DisableWarning();
+                    String name = edt_name.getText().toString().trim();
+                    String phone = edt_phone.getText().toString().trim();
+                    String email = edt_email.getText().toString().trim();
+                    String address = edt_addr.getText().toString().trim();
+                    BitmapDrawable bitmapDrawable = (BitmapDrawable) img_avt.getDrawable(); //lấy hình từ imageView
+                    //chuyển từ bitmapDrawable -> bitmap
+                    Bitmap bitmap = bitmapDrawable.getBitmap();
+                    //chuyển về mảng byte
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    //Định dạng lại kiểu dữ liệu trước khi xuất
+                    //(định dạng ảnh; quality (chất lượng) chạy từ 1 -> 100 càng nhỏ ảnh càng nét, 100 là mặc định; mảng byte)
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 1, byteArrayOutputStream);
+                    byte[] avatar = byteArrayOutputStream.toByteArray();
+
+                    database.InsertUser(new User(name, phone, email, address, avatar));
+                    Toast.makeText(RegisterAccountActivity.this, "Xin chào, " + name, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterAccountActivity.this, HomeActivity.class));
+                    finish();
+                }
+            }
+        });
     }
+
+    private Boolean CheckData() {
+        String regexEmail = "^[a-zA-Z][\\w-]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$";
+        String regexPhone = "^0\\d{9}$";
+        if (edt_name.getText().toString().trim().length() == 0){
+            tv_w_name.setVisibility(View.VISIBLE);
+            return false;
+        }
+        if (edt_phone.getText().toString().trim().length() == 0){
+            tv_w_phone.setVisibility(View.VISIBLE);
+            tv_w_r_phone.setVisibility(View.INVISIBLE);
+            return false;
+        }
+        if (!Pattern.matches(regexPhone, edt_phone.getText().toString().trim())){  //regex
+            tv_w_phone.setVisibility(View.INVISIBLE);
+            tv_w_r_phone.setVisibility(View.VISIBLE);
+            return false;
+        }
+        if (edt_email.getText().toString().trim().length() == 0){
+            tv_w_email.setVisibility(View.VISIBLE);
+            tv_w_r_email.setVisibility(View.INVISIBLE);
+            return false;
+        }
+        if (!Pattern.matches(regexEmail, edt_email.getText().toString().trim())){  //regex
+            tv_w_email.setVisibility(View.INVISIBLE);
+            tv_w_r_email.setVisibility(View.VISIBLE);
+            return false;
+        }
+        if (edt_addr.getText().toString().trim().length() == 0){
+            tv_w_addr.setVisibility(View.VISIBLE);
+            return false;
+        }
+        return true;
+    }
+
+    private void DisableWarning(){
+        tv_w_name.setVisibility(View.INVISIBLE);
+        tv_w_phone.setVisibility(View.INVISIBLE);
+        tv_w_email.setVisibility(View.INVISIBLE);
+        tv_w_addr.setVisibility(View.INVISIBLE);
+        tv_w_r_phone.setVisibility(View.INVISIBLE);
+        tv_w_r_email.setVisibility(View.INVISIBLE);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/qlbdt/fEnum/EDesSmartphoneDetail.java
@@ -0,0 +1,46 @@
+package com.example.qlbdt.fEnum;
+
+public enum EDesSmartphoneDetail {
+
+    DES_i13prm ("iPhone 13 Pro Max thuộc phân khúc điện thoại cao cấp mà không một iFan nào có " +
+            "thể bỏ qua, với màn hình lớn sắc nét, cấu hình vượt trội, dung lượng lưu trữ \"khủng\", thời gian sử dụng dài, mỗi lần trải nghiệm đều cho bạn cảm giác thỏa mãn đáng ngạc nhiên.\n"),
+    DES_i14prm ("Mẫu smartphone đến từ nhà Apple được mong đợi nhất năm 2022, lần này nhà Táo " +
+            "mang đến cho chúng ta một phiên bản với kiểu thiết kế hình notch mới, cấu hình mạnh mẽ nhờ con chip Apple A16 Bionic và cụm camera có độ phân giải được nâng cấp lên đến 48 MP."),
+    DES_i11 ("Apple đã trình làng bộ 3 siêu phẩm iPhone 11, trong đó phiên bản iPhone 11 64GB có mức giá rẻ nhất nhưng vẫn được nâng cấp mạnh mẽ như iPhone Xr ra mắt trước đó.\n"),
+    DES_i12 ("Smartphone iPhone 12 256 GB được Apple cho ra mắt đã đem đến làn sóng mạnh mẽ đối " +
+            "với những ai yêu công nghệ nói chung và “fan hâm mộ” trung thành của điện thoại iPhone nói riêng, với con chip mạnh, dung lượng lưu trữ lớn cùng thiết kế toàn diện và khả năng hiển thị hình ảnh xuất sắc."),
+    DES_i14p ("Sau nhiều thế hệ điện thoại của Apple thì cái tên “Plus” cũng đã chính thức trở " +
+            "lại vào năm 2022 và xuất hiện trên chiếc iPhone 14 Plus 128GB, nổi trội với ngoại hình bắt trend cùng màn hình kích thước lớn để đem đến không gian hiển thị tốt hơn cùng cấu hình mạnh mẽ không đổi so với bản tiêu chuẩn."),
+    DES_Nokia_G11 ("Nokia G11 được hãng cho ra mắt với hiệu năng ổn định, màn hình kích thước lớn" +
+            " mang đến những trải nghiệm giải trí tuyệt vời cùng thời gian sử dụng lâu dài với viên pin cực khủng."),
+    DES_Oppo_FindX5pro ("OPPO Find X5 Pro 5G có lẽ là cái tên sáng giá được xướng tên trong danh " +
+            "sách điện thoại có thiết kế ấn tượng trong năm 2022. Máy được hãng cho ra mắt với một diện mạo độc lạ chưa từng có, cùng với đó là những nâng cấp về chất lượng ở camera nhờ sự hợp tác với nhà sản xuất máy ảnh Hasselblad."),
+    DES_Oppo_Reno8pro ("OPPO Reno8 Pro 5G là chiếc điện thoại cao cấp được nhà OPPO ra mắt vào " +
+            "thời điểm 09/2022, máy hướng đến sự hoàn thiện cao cấp ở phần thiết kế cùng khả năng quay chụp chuyên nghiệp nhờ trang bị vi xử lý hình ảnh MariSilicon X chuyên dụng."),
+    DES_SS_ZFOLD3 ("Samsung Galaxy Z Fold3 5G đánh dấu bước tiến mới của hãng trong phân khúc " +
+            "điện thoại gập cao cấp khi được cải tiến về độ bền cùng những nâng cấp đáng giá về cấu hình hiệu năng, hứa hẹn sẽ mang đến trải nghiệm sử dụng đột phá cho người dùng."),
+    DES_SS_S20FE ("Samsung đã giới thiệu đến người dùng thành viên mới của dòng S20 Series đó " +
+            "chính là điện thoại Samsung Galaxy S20 FE. Đây là mẫu flagship cao cấp quy tụ nhiều tính năng mà Samfan yêu thích, hứa hẹn sẽ mang lại trải nghiệm cao cấp của dòng Galaxy S với mức giá dễ tiếp cận hơn."),
+    DES_SS_S23U ("Sau gần một năm chờ đợi thì siêu phẩm Samsung Galaxy S23 Ultra 5G cuối cùng " +
+            "cũng đã chính thức lộ diện với hàng loạt công nghệ dẫn đầu ngành điện thoại, nổi bật trong số đó chính là con chip Snapdragon 8 Gen 2, camera 200 MP và đi kèm với bút S Pen tích hợp để bạn có thể thoải mái vẽ vời hay ghi chú một cách nhanh chóng."),
+    DES_SS_ZFLIP4 ("Samsung Galaxy Z Flip4 128GB đã chính thức ra mắt thị trường công nghệ, đánh " +
+            "dấu sự trở lại của Samsung trên con đường định hướng người dùng về sự tiện lợi trên những chiếc điện thoại gập. Với độ bền được gia tăng cùng kiểu thiết kế đẹp mắt giúp Flip4 trở thành một trong những tâm điểm sáng giá cho nửa cuối năm 2022."),
+    DES_VV_V25pro ("Vivo V25 Pro 5G vừa được ra mắt với một mức giá bán cực kỳ hấp dẫn, thế mạnh " +
+            "của máy thuộc về phần hiệu năng nhờ trang bị con chip MediaTek Dimensity 1300 và cụm camera sắc nét 64 MP, hứa hẹn mang lại cho người dùng những trải nghiệm ổn định trong suốt quá trình sử dụng."),
+    DES_VV_Y21 ("Vivo Y21 chiếc smartphone mang trong mình nhiều ưu điểm nổi bật như màn hình " +
+            "viền mỏng đẹp mắt, hiệu năng ổn định cùng dung lượng pin tận 5000 mAh chắc chắn sẽ đáp ứng nhu cầu sử dụng của bạn cả ngày dài."),
+    DES_XM_12Tpro ("Cuối cùng Xiaomi 12T Pro 5G cũng đã chính thức lộ diện trên thị trường sau " +
+            "hàng loạt thông tin rò rỉ về thông số, đúng như dự đoán thì độ phân giải trên camera của phiên bản này được nhà sản xuất nâng cấp lên đến 200 MP, giúp máy trở thành thiết bị có khả năng ghi hình sắc nét thuộc top đầu giới smartphone, đi kèm với đó là màn hình chất lượng cùng bộ vi xử lý mạnh mẽ xứng tầm flagship."),
+    DES_XM_N11 ("Redmi Note 11 (6GB/128GB) vừa được Xiaomi cho ra mắt, được xem là chiếc " +
+            "smartphone có giá tầm trung ấn tượng, với 1 cấu hình mạnh, cụm camera xịn sò, pin khỏe, sạc nhanh mà giá lại rất phải chăng.");
+
+    public String getDes() {
+        return des;
+    }
+
+    EDesSmartphoneDetail(String des) {
+        this.des = des;
+    }
+
+    private String des;
+}
