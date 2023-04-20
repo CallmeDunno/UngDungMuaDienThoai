06a861abeaf2ffc3566c3d4acf1028500f209a4e
phamtung240402 <phamtung240402@gmail.com>
2023-04-20 16:14:01 +0700

    Tung commit

---
+++ app/src/main/java/com/example/qlbdt/fActivity/Login/LogInActivity.java
@@ -0,0 +1,27 @@
+package com.example.qlbdt.fActivity.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.qlbdt.R;
+import com.example.qlbdt.fFragment.LogInPackage.LogInFragment;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        replaceFragment(new LogInFragment());
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_login, fragment);
+        fragmentTransaction.commit();
+    }
+}
