74823ce67501a197da8993f8e4561bca4ef327af
The Girl <phamdungabcdef@gmail.com>
2023-04-24 20:21:38 +0700

    Tien Dung Search 3

--- app/src/main/java/com/example/qlbdt/fFragment/Search/SearchFragment.java
+++ app/src/main/java/com/example/qlbdt/fFragment/Search/SearchFragment.java
@@ -3,14 +3,20 @@ package com.example.qlbdt.fFragment.Search;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -26,16 +32,21 @@ import com.example.qlbdt.fInterface.IRecyclerViewOnClick;
 import com.example.qlbdt.fObject.Photo;
 import com.example.qlbdt.fObject.Smartphone;
 import com.example.qlbdt.fObject.User2;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.xml.transform.sax.TemplatesHandler;
 
@@ -54,6 +65,7 @@ public class SearchFragment extends Fragment {
     private Spinner sp_sort, sp_brand;
     private ArrayList<String> sort, brand;
     private  ArrayAdapter adapterSort, adapterBrand;
+    private EditText et_search;
 
 
     @Override
@@ -64,6 +76,8 @@ public class SearchFragment extends Fragment {
 
         initUI();
         searchViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
+        searchViewModel.fetchBrandList();
+
         searchViewModel.getListProductSearchLiveData().observe(getActivity(), new Observer<List<ProductSearch>>() {
             @Override
             public void onChanged(List<ProductSearch> productSearches) {
@@ -80,10 +94,7 @@ public class SearchFragment extends Fragment {
         sp_sort.setAdapter(adapterSort);
 
         sp_brand = view.findViewById(R.id.sp_brand_fragment_search);
-        brand = new ArrayList<>();
-        brand.add("Hãng");
-        adapterBrand = new ArrayAdapter(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, brand);
-        sp_brand.setAdapter(adapterBrand);
+        Brand();
 
         return view;
     }
@@ -102,7 +113,28 @@ public class SearchFragment extends Fragment {
 
         binding.rcvSearch.setAdapter(producSearchAdapter);
     }
+    private void Brand() {
+        adapterBrand = new ArrayAdapter<>(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        sp_brand.setAdapter(adapterBrand);
+        searchViewModel.getBrandListLiveData().observe(getViewLifecycleOwner(), brandList -> {
+            adapterBrand.clear();
+            adapterBrand.addAll(brandList);
+        });
+        sp_brand.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedBrand = (String) adapterBrand.getItem(i);
+                if (!selectedBrand.equals("Hãng")) {
+                    searchViewModel.fetchProductByBrand(selectedBrand);
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
+
+    }
 
 }
--- app/src/main/java/com/example/qlbdt/fFragment/Search/SearchViewModel.java
+++ app/src/main/java/com/example/qlbdt/fFragment/Search/SearchViewModel.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -21,19 +22,46 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SearchViewModel extends ViewModel {
 
     private MutableLiveData<List<ProductSearch>> listProductSearchLiveData;
-
+    private MutableLiveData<List<String>> brandListLiveData;
     private List<ProductSearch> listProductSearch;
 
 
     public SearchViewModel() {
         listProductSearchLiveData = new MutableLiveData<>();
-
+        brandListLiveData = new MutableLiveData<>();
         initData();
+    }
+    public LiveData<List<String>> getBrandListLiveData() {
+        return brandListLiveData;
+    }
+    public void fetchBrandList() {
+        List<String> brandList = new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Products")
+                .whereEqualTo("type", "Smartphone")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Set<String> brandSet = new HashSet<>();
+                        for (QueryDocumentSnapshot doc : task.getResult()) {
+                            String brand = doc.toObject(ProductSearch.class).getBrand();
+                            brandSet.add(brand);
+                        }
+                        brandList.addAll(brandSet);
+                        Collections.sort(brandList);
+                        brandList.add(0, "Hãng");
+                        brandListLiveData.setValue(brandList);
+                    } else {
+                        Log.e("SearchViewModel", "Error fetching brand list", task.getException());
+                    }
+                });
     }
 
     private void initData() {
@@ -56,7 +84,6 @@ public class SearchViewModel extends ViewModel {
                                 listProductSearch.add(new ProductSearch(name, price, image));
                             }
                             Collections.shuffle(listProductSearch);
-
                             listProductSearchLiveData.postValue(listProductSearch);
 
                         } else {
@@ -69,7 +96,30 @@ public class SearchViewModel extends ViewModel {
 
     }
 
+
     public MutableLiveData<List<ProductSearch>> getListProductSearchLiveData() {
         return listProductSearchLiveData;
+    }
+
+    public void fetchProductByBrand(String brand) {
+        List<ProductSearch> productList = new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Products")
+                .whereEqualTo("type", "Smartphone")
+                .whereEqualTo("brand", brand)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot doc : task.getResult()) {
+                            String name = doc.toObject(ProductSearch.class).getName();
+                            String price = doc.toObject(ProductSearch.class).getPrice();
+                            String image = doc.toObject(ProductSearch.class).getImage();
+                            productList.add(new ProductSearch(name, price, image));
+                        }
+                        listProductSearchLiveData.postValue(productList);
+                    } else {
+                        Log.e("SearchViewModel", "Error fetching product list by brand", task.getException());
+                    }
+                });
     }
 }
